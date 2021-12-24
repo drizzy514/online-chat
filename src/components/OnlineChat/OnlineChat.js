be3b97644051cb6be2b7eb82f8cd4866a1342c4a
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-24 21:01:55 +0500

    Update OnlineChat.js

--- src/components/OnlineChat/OnlineChat.js
+++ src/components/OnlineChat/OnlineChat.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 import Chat from "./Chat";
 import { Link } from "react-router-dom";
 
-const socket = io.connect("https://devcenter.heroku.com/articles/buildpacks");
+const socket = io.connect("https://localhost:2020");
 
 function OnlineChat() {
   const [username, setUsername] = useState("");
